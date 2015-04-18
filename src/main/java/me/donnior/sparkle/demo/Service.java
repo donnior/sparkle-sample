14664cf4c8072d0860c7662a9e8b3a69e89b1e70
Donny <donnior@gmail.com>
2015-04-18 13:28:48 +0800

    new sparkle sample project

---
+++ src/main/java/me/donnior/sparkle/demo/Service.java
@@ -0,0 +1,26 @@
+package me.donnior.sparkle.demo;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.google.inject.Singleton;
+
+@Singleton
+public class Service {
+
+    private List<User> users = new ArrayList<User>();
+    
+    public void hello(){
+        System.out.println("hello");
+    }
+    
+    public int saveUser(User user) {
+        this.users.add(user);
+        return this.users.size();
+    }
+    
+    public List<User> listUsers(){
+        return this.users;
+    }
+    
+}
---
+++ src/main/webapp/WEB-INF/projects/index2.jsp
@@ -0,0 +1,4 @@
+<%@ taglib prefix="r" uri="r-tag"%>
+<r:layout>
+    <h2>This is project index 2 page</h2>
+</r:layout>
