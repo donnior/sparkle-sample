785357b21c2a4824fa1835dd029a54a8f9116a64
Donny <donnior@gmail.com>
2015-07-03 16:12:07 +0800

    update

--- mini-sample/src/main/java/demo/DemoRouteModule.java
+++ mini-sample/src/main/java/demo/DemoRouteModule.java
@@ -9,10 +9,11 @@ public class DemoRouteModule implements RouteModule {
     @Override
     public void config(Router router) {
         
-        router.match("/test").to(request -> {
+        router.match("/test/{id}").to(request -> {
             return e -> {
                 e.expose(200).withName("status");
                 e.expose("OK").withName("msg");
+                e.expose(request.pathVariable("id")).withName("id");
             };
         });
     
